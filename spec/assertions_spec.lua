5372e96f7d10b47038047ae7b15e6a917ff7f9e5
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-12-16 23:38:37 +0100

    added test for issue 30; https://github.com/Olivine-Labs/luassert/issues/30

--- spec/assertions_spec.lua
+++ spec/assertions_spec.lua
@@ -4,7 +4,13 @@ describe("Test Assertions", function()
     local table2 = { derp = false}
     assert.same(table1, table2)
   end)
-
+  
+  it("Checks the same() assertion for tables with protected metatables", function()
+    local troubleSomeTable = {}
+    setmetatable(troubleSomeTable, {__metatable = 0})
+    assert.are.same(troubleSomeTable, troubleSomeTable)
+  end)
+  
   it("Checks same() assertion to handle nils properly", function()
     assert.is.error(function() assert.same(nil) end)  -- minimum 2 arguments
     assert.same(nil, nil)
