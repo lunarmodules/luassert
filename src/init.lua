12525e3fac215a57d32be835904842ff121e1505
mpeterv <mpeterval@gmail.com>
2015-03-17 15:40:54 +0300

    Update version in init.lua
    Fixes #56

--- src/init.lua
+++ src/init.lua
@@ -2,7 +2,7 @@ local assert = require('luassert.assert')
 
 assert._COPYRIGHT   = "Copyright (c) 2012 Olivine Labs, LLC."
 assert._DESCRIPTION = "Extends Lua's built-in assertions to provide additional tests and the ability to create your own."
-assert._VERSION     = "Luassert 1.4"
+assert._VERSION     = "Luassert 1.7.5"
 
 -- load basic asserts
 require('luassert.assertions')
