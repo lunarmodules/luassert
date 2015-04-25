b545320b6d4d9c32c161afb0c9ff87b8cf1652fe
Oscar Lim <olim@ucla.edu>
2015-04-25 01:56:01 -0700

    Add support for reverting a mock

--- src/mock.lua
+++ src/mock.lua
@@ -1,27 +1,48 @@
--- module will return a single mock function, no table nor register any assertions
+-- module will return a mock module table, and will not register any assertions
 local spy = require 'luassert.spy'
 local stub = require 'luassert.stub'
 
-local function mock(object, dostub, func, self, key)
-  local data_type = type(object)
-  if data_type == "table" then
-    if spy.is_spy(object) then
-      -- this table is a function already wrapped as a spy, so nothing to do here
-    else
-      for k,v in pairs(object) do
-        object[k] = mock(v, dostub, func, object, k)
+local mock
+mock = {
+  new = function(object, dostub, func, self, key)
+    local data_type = type(object)
+    if data_type == "table" then
+      if spy.is_spy(object) then
+        -- this table is a function already wrapped as a spy, so nothing to do here
+      else
+        for k,v in pairs(object) do
+          object[k] = mock.new(v, dostub, func, object, k)
+        end
+      end
+    elseif data_type == "function" then
+      if dostub then
+        return stub(self, key, func)
+      elseif self==nil then
+        return spy.new(object)
+      else
+        return spy.on(self, key)
       end
     end
-  elseif data_type == "function" then
-    if dostub then
-      return stub(self, key, func)
-    elseif self==nil then
-      return spy.new(object)
-    else
-      return spy.on(self, key)
-    end
-  end
-  return object
-end
+    return object
+  end,
 
-return mock
+  revert = function(object)
+    if type(object) ~= "table" then return end
+    if spy.is_spy(object) then
+      object:revert()
+      return
+    end
+    for k,v in pairs(object) do
+      mock.revert(v)
+    end
+    return object
+  end
+}
+
+return setmetatable(mock, {
+  __call = function(self, ...)
+    -- mock originally was a function only. Now that it is a module table
+    -- the __call method is required for backward compatibility
+    return mock.new(...)
+  end
+})
