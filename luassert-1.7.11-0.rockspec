ba9e2f89f61692f3634bc5eea366abd1bfcbce3f
Jack Lawson <ajacksified@gmail.com>
2018-09-24 10:11:48 -0400

    v1.7.11 release

--- luassert-1.7.11-0.rockspec
+++ luassert-1.7.11-0.rockspec
@@ -1,8 +1,8 @@
 package = "luassert"
-version = "1.7.10-0"
+version = "1.7.11-0"
 source = {
-  url = "https://github.com/Olivine-Labs/luassert/archive/v1.7.10.tar.gz",
-  dir = "luassert-1.7.10"
+  url = "https://github.com/Olivine-Labs/luassert/archive/v1.7.11.tar.gz",
+  dir = "luassert-1.7.11"
 }
 description = {
   summary = "Lua Assertions Extension",
--- src/init.lua
+++ src/init.lua
@@ -1,8 +1,8 @@
 local assert = require('luassert.assert')
 
-assert._COPYRIGHT   = "Copyright (c) 2012 Olivine Labs, LLC."
+assert._COPYRIGHT   = "Copyright (c) 2018 Olivine Labs, LLC."
 assert._DESCRIPTION = "Extends Lua's built-in assertions to provide additional tests and the ability to create your own."
-assert._VERSION     = "Luassert 1.7.9"
+assert._VERSION     = "Luassert 1.7.11"
 
 -- load basic asserts
 require('luassert.assertions')
